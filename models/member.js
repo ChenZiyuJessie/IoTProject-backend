451ed82d16b91803351349dc354ef11d76693855
ChenZiyuJessie <250714751@qq.com>
2018-04-21 00:03:36 +0700

    member crud

--- models/member.js
+++ models/member.js
@@ -9,7 +9,16 @@ var MemberSchema = new Schema({
     name: {
         type: String,
         required: true
-    }
+    },
+    tel:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    } 
+
 });
 
 module.exports = mongoose.model('Member', MemberSchema);
