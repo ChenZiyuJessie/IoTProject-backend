7fb01c3f77f5bb288f7e9900c512dfd7c0f77fd2
ChenZiyuJessie <250714751@qq.com>
2018-04-22 16:10:10 +0700

    delete

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var logger = require('morgan');
 var bodyParser = require('body-parser');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var memberRouter=require('./routes/members');
 var cors = require('cors');
 
 var app = express();
@@ -53,7 +52,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/member',memberRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
